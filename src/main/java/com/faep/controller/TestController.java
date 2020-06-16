edabba691c7db1c4e93d337009abe00291f03525
Faep1008 <faep327@126.com>
2020-06-16 16:02:35 +0800

    add log4j2

--- src/main/java/com/faep/controller/TestController.java
+++ src/main/java/com/faep/controller/TestController.java
@@ -1,8 +1,16 @@
 package com.faep.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 
 /**
  * 作者： Faep
@@ -15,8 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test(){
+        logger.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "-接口被调用");
         return "111ds进行开发";
     }
 
