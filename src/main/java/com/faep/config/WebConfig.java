b807064fc42cc3d7efb2868a3ad171ede1f0c5bc
Faep1008 <faep327@126.com>
2020-07-24 17:37:35 +0800

    20200724

--- src/main/java/com/faep/config/WebConfig.java
+++ src/main/java/com/faep/config/WebConfig.java
@@ -1,9 +1,17 @@
 package com.faep.config;
 
+import com.alibaba.druid.support.http.StatViewServlet;
 import com.faep.filter.WebFilter;
+import com.faep.vo.DruidConfigVo;
+import org.atmosphere.cpr.AtmosphereServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 描述： [ ]
@@ -15,8 +23,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebConfig {
 
+    @Autowired
+    private DruidConfigVo druidConfigVo;
+
     /**
-     * 过滤器
+     * 注册过滤器
      * @return
      */
     @Bean
@@ -26,6 +37,41 @@ public class WebConfig {
         registration.setName("WebFilter");
         registration.setAsyncSupported(true);
         return registration;
+    }
+
+    /**
+     * 注册WebSocket拦截器
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        // 第一个参数是我们自己的Servlet对象
+        // 第二个参数是是对应的路径，可以写多个
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new AtmosphereServlet(), "/websocket");
+        servletRegistrationBean.addInitParameter("org.atmosphere.cpr.packages","com.faep.websocket.WebSocketServer");
+        servletRegistrationBean.addInitParameter("org.atmosphere.interceptor.HeartbeatInterceptor.clientHeartbeatFrequencyInSeconds", "5");
+        servletRegistrationBean.setLoadOnStartup(0);
+        servletRegistrationBean.setAsyncSupported(true);
+        return servletRegistrationBean;
+    }
+
+    /**
+     * Druid配置
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings(druidConfigVo.getUrlmappings());
+        Map<String, String> initParameters = new HashMap<>();
+        initParameters.put("resetEnable", druidConfigVo.getResetenable()); //禁用HTML页面上的“Rest All”功能
+        initParameters.put("allow", druidConfigVo.getAllow());  //ip白名单（没有配置或者为空，则允许所有访问）
+        initParameters.put("loginUsername", druidConfigVo.getLoginusername());  //++监控页面登录用户名
+        initParameters.put("loginPassword", druidConfigVo.getLoginpassword());  //++监控页面登录用户密码
+        initParameters.put("deny", druidConfigVo.getDeny()); //ip黑名单，如果某个ip同时存在，deny优先于allow
+        servletRegistrationBean.setInitParameters(initParameters);
+        return servletRegistrationBean;
     }
 
 }
